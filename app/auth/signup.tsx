598a0379b3410d8c6124efb4e5ec226c4b5ac841
abdulazeezsodiq <sodiq.asynctechs@gmail.com>
2024-12-26 12:17:48 +0100

    bug fixed

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -16,7 +16,7 @@ const SignUpForm: React.FC = () => {
       .min(6, "Password must be at least 6 characters")
       .required("Password is required"),
     confirmPassword: Yup.string()
-      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .oneOf([Yup.ref("password")], "Passwords must match")
       .required("Confirm Password is required"),
   });
 
